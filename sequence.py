2a71ea7d9496acb7124b5dd1059c4fb4f474c5ef
AIXUH23 <aixuh23@gmail.com>
2022-02-08 22:34:13 +0900

    commit

--- sequence.py
+++ sequence.py
@@ -1,23 +1,23 @@
 n, k = map(int, input().split(" "))
 nList = list(map(int, input().split()))
 
-temp = sum(nList[:k])
-MaxSum = temp
+temp = 0
+sum = 0
+MaxSum = 0
 
-for i in range(k, len(nList)-k+1):
-    temp += nList[i] - nList[i-k]
-    MaxSum = max(MaxSum, temp)
-
+for i in range(len(nList)-k+1):
+    sum += nList[i]
+    if i-temp+1 == k:
+        MaxSum = max(MaxSum, sum)
+        sum -= nList[temp]
+        temp += 1
 print(MaxSum)
 
-# temp = 0
-# sum = 0
-# MaxSum = 0
+# temp = sum(nList[:k])
+# MaxSum = temp
 
-# for i in range(len(nList)-k+1):
-#     sum += nList[i]
-#     if i-temp+1 == k:
-#         MaxSum = max(MaxSum, sum)
-#         sum -= nList[temp]
-#         temp += 1
+# for i in range(k, len(nList)-k+1):
+#     temp += nList[i] - nList[i-k]
+#     MaxSum = max(MaxSum, temp)
+
 # print(MaxSum)
